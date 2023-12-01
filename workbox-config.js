99fd5297200dcb7c5afbd527462a35ad794068a8
shamia-code <shamienamagembegmail.com>
2023-12-01 11:59:37 +0300

    initial commit

---
+++ workbox-config.js
@@ -0,0 +1,12 @@
+module.exports = {
+	globDirectory: './',
+	globPatterns: [
+		'**/*.{css,html,js}'
+	],
+	swDest: 'sw.js',
+	ignoreURLParametersMatching: [
+		/^utm_/,
+		/^fbclid$/,
+		/^n/
+	]
+};
